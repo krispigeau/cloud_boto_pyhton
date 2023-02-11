55f0c904486e5c3524c2480756b5a2aa40039958
EC2 Default User <ec2-user@ip-172-31-16-68.ec2.internal>
2023-02-11 18:56:24 +0000

    added python script to automate creating ec2 instances

---
+++ create_ec2.py
@@ -0,0 +1,23 @@
+import boto3
+
+# Variables receive values from the user:
+quantity = int(input('Enter the number of EC2s to be created: '))
+ami = (input('Enter the Image ID: '))
+keyname = (input('Enter the key name: '))
+
+# This function creates EC2 instance:
+def create_instance():
+ ec2_client = boto3.client("ec2", region_name="us-east-1")
+ instances = ec2_client.run_instances(
+    ImageId = ami,
+    MinCount = quantity,
+    MaxCount = quantity,
+    InstanceType = "t2.micro",
+    KeyName = keyname,
+    TagSpecifications=[{'ResourceType': 'instance','Tags': [{'Key': 'Name','Value': 'VM'}]}])
+
+# The created instances ID are printed:
+ for n in range(quantity):
+    print(instances["Instances"][n]["InstanceId"])
+ 
+create_instance()
