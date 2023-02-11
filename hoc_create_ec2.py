0277c359532f58cd685588b8ed305b5802f62476
Kris Pigeau <kris.pigeua@gmail.com>
2023-02-11 20:57:03 +0000

    remove instance thing and added my variables

--- hoc_create_ec2.py
+++ hoc_create_ec2.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 import boto3
 AWS_REGION = "us-east-1"
-KEY_PAIR_NAME = 'Lenovo T410'
+KEY_PAIR_NAME = "kris_desktop"
 AMI_ID = 'ami-0c02fb55956c7d316' # Amazon Linux 2
-SUBNET_ID = 'subnet-0984555689f5894d8'
-SECURITY_GROUP_ID = 'sg-01304974040835e2f'
-INSTANCE_PROFILE = 'EC2-Admin'
+SUBNET_ID = 'subnet-0e30cfd012330e1db'
+SECURITY_GROUP_ID = 'sg-0135c8e5aa0fb0553'
 USER_DATA = '''#!/bin/bash
 yum update
 '''
-EC2_RESOURCE = boto3.resource('ec2', region_name=AWS_REGION)
-EC2_CLIENT = boto3.client('ec2', region_name=AWS_REGION)
+EC2_RESOURCE = boto3.resource("ec2", region_name="us-east-1")
 instances = EC2_RESOURCE.create_instances(
     MinCount = 1,
     MaxCount = 1,
@@ -35,12 +33,5 @@ instances = EC2_RESOURCE.create_instances(
 
 for instance in instances:
     print(f'EC2 instance "{instance.id}" has been launched')
-    
     instance.wait_until_running()
-    
-    EC2_CLIENT.associate_iam_instance_profile(
-        IamInstanceProfile = {'Name': INSTANCE_PROFILE},
-        InstanceId = instance.id,
-    )
-    print(f'EC2 Instance Profile "{INSTANCE_PROFILE}" has been attached')
     print(f'EC2 instance "{instance.id}" has been started')
