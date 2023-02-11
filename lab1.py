afd024dcbfd4b348018c1e3cfa01b3e048e582aa
Kris Pigeau <kris.pigeua@gmail.com>
2023-02-11 21:30:13 +0000

    added fuction to create ec2 instances

--- lab1.py
+++ lab1.py
@@ -16,5 +16,42 @@ subnetIDs = []
 # Add subnet IDs to list
 for subnet in response['Subnets']:
     subnetIDs.append(subnet['SubnetId'])
+
+def create_instance(ID):
+    KEY_PAIR_NAME = "kris_desktop"
+    AMI_ID = 'ami-0c02fb55956c7d316' # Amazon Linux 2
+    SUBNET_ID = ID
+    SECURITY_GROUP_ID = "sg-0135c8e5aa0fb0553"
+    USER_DATA = '''#!/bin/bash
+    yum update
+    '''
+    EC2_RESOURCE = boto3.resource("ec2", region_name="us-east-1")
+    instances = EC2_RESOURCE.create_instances(
+        MinCount = 1,
+        MaxCount = 1,
+        ImageId=AMI_ID,
+        InstanceType='t2.micro',
+        KeyName=KEY_PAIR_NAME,
+        SecurityGroupIds = [SECURITY_GROUP_ID],
+        SubnetId=SUBNET_ID,
+        UserData=USER_DATA,
+        TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {
+                        'Key': 'Name',
+                        'Value': 'VM'
+                    },
+                ]
+            },
+        ]
+    )
+
+    for instance in instances:
+        print(f'EC2 instance "{instance.id}" has been launched')
+
+
+for subID in subnetIDs:
+    create_instance(subID)
     
-print(subnetIDs)
